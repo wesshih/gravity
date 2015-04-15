9bc8fb0f66dd3032ab57aa055f770d925323867c
Wes Shih <wesshih@github.com>
2015-04-15 09:03:23 -0700

    getting closer with gui graphing

--- python/simulation.py
+++ python/simulation.py
@@ -4,9 +4,13 @@ from gravity import *
 import numpy as np
 from operator import *
 
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import cnames
+from matplotlib import animation
 
-numP = 10
-numT = 1000
+numP = 2
+numT = 50
 
 ps = []
 for i in range(numP):
@@ -47,9 +51,48 @@ simForT(ps, numT, x_t)
 #print x_t
 print "done"
 
-#print ps
-#moveParticles(ps)
-#print ps
-#updateVelocity(ps)
-#print ps
-#print x_t
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1], projection='3d')
+ax.axis('off')
+
+colors = plt.cm.jet(np.linspace(0,1,numP))
+
+
+ax.set_xlim((-10, 10))
+ax.set_ylim((-10, 10))
+ax.set_zlim((1, 20))
+ax.view_init(30,0)
+
+
+theta = np.linspace(-4 * np.pi, 4*np.pi, 100)
+z = np.linspace(-2, 2, 100)
+r = z**2 + 1
+x = r * np.sin(theta)
+y = r * np.cos(theta)
+line = ax.plot(x, y, z, label='parametric curve')
+
+#lines = sum([ax.plot([], [], [], '-', c=c) for c in colors], [])
+
+#def init():
+#  #for line in lines:
+#  line_a.set_data([], [])
+#  line_a.set_3d_properties([])
+#  return line_a
+
+#def animate(i):
+#  #x(t) = 3t+1
+#  #y(t) = 0.25t-1
+#  #z(t) = -t+0.5
+#  #for line in lines:
+#  x = i/10
+#  y = i/10
+#  z = i/10
+#  line_a.set_data(x, y)
+#  line_a.set_3d_properties(z)
+#  ax.view_init(30, 0.5 * i)
+#  fig.canvas.draw()
+#  return line_a
+
+#anim = animation.FuncAnimation(fig, animate, init_func=init, frames=500, interval=15, blit=True)
+
+plt.show()
